800f8bcad13a76541528ddcd1a7f1037309ddf5b
Chris Finlayson <chris@intellitect.com>
2025-03-10 18:46:32 -0700

    style/length inputs

--- Move.Engine.Web/src/views/Home.vue
+++ Move.Engine.Web/src/views/Home.vue
@@ -1,7 +1,41 @@
 <template>
   <div class="home">
     <c-loader-status :loaders="[workoutService.generateWorkout]" />
-    <v-textarea v-model="workoutRequest"></v-textarea>
+
+    <v-tabs v-model="tab">
+      <v-tab value="equipment">Equipment</v-tab>
+      <v-tab value="style">Style</v-tab>
+      <v-tab value="length">Length</v-tab>
+    </v-tabs>
+
+    <v-window v-model="tab">
+      <v-window-item value="equipment">
+        <v-select
+          v-model="selectedEquipment"
+          label="Select Equipment"
+          :items="equipmentOptions"
+          multiple
+        ></v-select>
+      </v-window-item>
+
+      <v-window-item value="style">
+        <v-select
+          v-model="selectedStyle"
+          label="Workout Style"
+          :items="workoutStyles"
+        ></v-select>
+      </v-window-item>
+
+      <v-window-item value="length">
+        <v-text-field
+          v-model="length"
+          label="Workout Length (minutes)"
+          type="number"
+        ></v-text-field>
+      </v-window-item>
+    </v-window>
+
+    <v-textarea v-model="editableRequest"></v-textarea>
     <div v-html="renderedMarkdown"></div>
     <v-btn @click="submitRequest">Generate Workout</v-btn>
   </div>
@@ -11,12 +45,57 @@
 import { WorkoutServiceViewModel } from "@/viewmodels.g";
 import MarkdownIt from "markdown-it";
 
-const workoutRequest = ref<string>(
-  "Generate a 45 minute workout using kettlebells, dumbbells and a jump rope.  Don't repeat any exercises.",
-);
+const workoutStyles = [
+  "CrossFit",
+  "Bodyweight (Calisthenics)",
+  "HIIT (High-Intensity Interval Training)",
+  "Strength Training (Weightlifting)",
+  "Circuit Training",
+  "Cardio (Running, Cycling)",
+  "Yoga",
+  "Functional Training",
+  "Powerlifting",
+  "Olympic Weightlifting",
+  "Tabata",
+];
+
+const equipmentOptions = [
+  "Kettlebells",
+  "Dumbbells",
+  "Barbells",
+  "Jump Rope",
+  "Resistance Bands",
+  "Medicine Ball",
+  "Pull-Up Bar",
+  "Bench",
+];
+
 const markdown = new MarkdownIt();
 const workoutService = new WorkoutServiceViewModel();
-const generatedWorkout = ref<string>("sdsdfsdf");
+const generatedWorkout = ref<string>("");
+const tab = ref("equipment");
+const length = ref<number | null>(null);
+const selectedStyle = ref<string | null>(null);
+const selectedEquipment = ref<string[]>([]);
+
+const editableRequest = computed({
+  get: () => {
+    let request = "Generate a workout.";
+    if (selectedEquipment.value.length) {
+      request = `using ${selectedEquipment.value.join(", ")}. ${request}`;
+    }
+    if (selectedStyle.value) {
+      request = `${selectedStyle.value} workout. ${request}`;
+    }
+    if (length.value) {
+      request = `${length.value} minute ${request}`;
+    }
+    return `${request} Don't repeat any exercises.`;
+  },
+  set(value) {
+    editableRequest.value = value;
+  },
+});
 
 const renderedMarkdown = computed(() =>
   markdown.render(generatedWorkout.value),
@@ -24,9 +103,10 @@ const renderedMarkdown = computed(() =>
 
 const submitRequest = async () => {
   generatedWorkout.value = await workoutService.generateWorkout(
-    workoutRequest.value,
+    editableRequest.value,
   );
   alert(generatedWorkout.value);
 };
-useTitle("Home");
+
+useTitle("Workout Generator");
 </script>
